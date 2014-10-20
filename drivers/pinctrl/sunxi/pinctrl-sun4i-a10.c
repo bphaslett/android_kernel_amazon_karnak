8cefc00568181449f3a495892c28c62f79a697de
Wolfram Sang <wsa@the-dreams.de>
2014-10-20 16:21:25 +0200

    pinctrl: sunxi: drop owner assignment from platform_drivers
    A platform_driver does not need to set an owner, it will be populated by the
    driver core.
    
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/pinctrl/sunxi/pinctrl-sun4i-a10.c
+++ drivers/pinctrl/sunxi/pinctrl-sun4i-a10.c
@@ -1030,7 +1030,6 @@ static struct platform_driver sun4i_a10_pinctrl_driver = {
 	.probe	= sun4i_a10_pinctrl_probe,
 	.driver	= {
 		.name		= "sun4i-pinctrl",
-		.owner		= THIS_MODULE,
 		.of_match_table	= sun4i_a10_pinctrl_match,
 	},
 };
