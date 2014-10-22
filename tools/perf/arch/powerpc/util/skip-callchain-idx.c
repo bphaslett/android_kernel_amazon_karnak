79b5ba21480e392d116de74359eaa57caa6db0a9
Sukadev Bhattiprolu <sukadev@linux.vnet.ibm.com>
2014-10-21 17:09:58 -0700

    perf tools powerpc: Cache the DWARF debug info
    Cache the DWARF debug info for DSO so we don't have to rebuild it for each
    address in the DSO.
    
    Note that dso__new() uses calloc() so don't need to set dso->dwfl to NULL.
    
    	$ /tmp/perf.orig --version
    	perf version 3.18.rc1.gc2661b8
    	$ /tmp/perf.new --version
    	perf version 3.18.rc1.g402d62
    	$ perf stat -e cycles,instructions /tmp/perf.orig report -g > orig
    
    	 Performance counter stats for '/tmp/perf.orig report -g':
    
    	     6,428,177,183 cycles                    #    0.000 GHz
    	     4,176,288,391 instructions              #    0.65  insns per cycle
    
    	       1.840666132 seconds time elapsed
    
    	$ perf stat -e cycles,instructions /tmp/perf.new report -g > new
    
    	 Performance counter stats for '/tmp/perf.new report -g':
    
    	       305,773,142 cycles                    #    0.000 GHz
    	       276,048,272 instructions              #    0.90  insns per cycle
    
    	       0.087693543 seconds time elapsed
    	$ diff orig new
    	$
    
    Changelog[v2]:
    
    [Arnaldo Carvalho] Cache in existing global objects rather than create
                       new static/globals in functions.
    
    Reported-by: Anton Blanchard <anton@samba.org>
    Signed-off-by: Sukadev Bhattiprolu <sukadev@linux.vnet.ibm.com>
    Cc: Anton Blanchard <anton@au1.ibm.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Link: http://lkml.kernel.org/r/20141022000958.GB2228@us.ibm.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/arch/powerpc/util/skip-callchain-idx.c
+++ tools/perf/arch/powerpc/util/skip-callchain-idx.c
@@ -149,7 +149,7 @@ static Dwarf_Frame *get_dwarf_frame(Dwfl_Module *mod, Dwarf_Addr pc)
  *		yet used)
  *	-1 in case of errors
  */
-static int check_return_addr(const char *exec_file, Dwarf_Addr pc)
+static int check_return_addr(struct dso *dso, Dwarf_Addr pc)
 {
 	int		rc = -1;
 	Dwfl		*dwfl;
@@ -160,15 +160,27 @@ static int check_return_addr(const char *exec_file, Dwarf_Addr pc)
 	Dwarf_Addr	end = pc;
 	bool		signalp;
 
-	dwfl = dwfl_begin(&offline_callbacks);
-	if (!dwfl) {
-		pr_debug("dwfl_begin() failed: %s\n", dwarf_errmsg(-1));
-		return -1;
-	}
+	dwfl = dso->dwfl;
 
-	if (dwfl_report_offline(dwfl, "",  exec_file, -1) == NULL) {
-		pr_debug("dwfl_report_offline() failed %s\n", dwarf_errmsg(-1));
-		goto out;
+	if (!dwfl) {
+		dwfl = dwfl_begin(&offline_callbacks);
+		if (!dwfl) {
+			pr_debug("dwfl_begin() failed: %s\n", dwarf_errmsg(-1));
+			return -1;
+		}
+
+		if (dwfl_report_offline(dwfl, "", dso->long_name, -1) == NULL) {
+			pr_debug("dwfl_report_offline() failed %s\n",
+						dwarf_errmsg(-1));
+			/*
+			 * We normally cache the DWARF debug info and never
+			 * call dwfl_end(). But to prevent fd leak, free in
+			 * case of error.
+			 */
+			dwfl_end(dwfl);
+			goto out;
+		}
+		dso->dwfl = dwfl;
 	}
 
 	mod = dwfl_addrmodule(dwfl, pc);
@@ -198,7 +210,6 @@ static int check_return_addr(const char *exec_file, Dwarf_Addr pc)
 	rc = check_return_reg(ra_regno, frame);
 
 out:
-	dwfl_end(dwfl);
 	return rc;
 }
 
@@ -250,7 +261,7 @@ int arch_skip_callchain_idx(struct machine *machine, struct thread *thread,
 		return skip_slot;
 	}
 
-	rc = check_return_addr(dso->long_name, ip);
+	rc = check_return_addr(dso, ip);
 
 	pr_debug("DSO %s, nr %" PRIx64 ", ip 0x%" PRIx64 "rc %d\n",
 				dso->long_name, chain->nr, ip, rc);
