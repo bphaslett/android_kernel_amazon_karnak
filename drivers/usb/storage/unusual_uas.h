19c5181398e0e1dff05ff1c735a388f0dd3842b5
Hans de Goede <hdegoede@redhat.com>
2015-01-08 15:15:14 +0100

    Resolve merge conflict for commit 078fd7d6308a
    uas: Do not blacklist ASM1153 disk enclosures
    
    Our detection logic to avoid doing UAS on ASM1051 bridge chips causes problems
    with newer ASM1153 disk enclosures in 2 ways:
    
    1) Some ASM1153 disk enclosures re-use the ASM1051 device-id of 5106, which
       we assume is always an ASM1051, so remove the quirk for 5106, and instead
       use the same detection logic as we already use for device-id 55aa, which is
       used for all of ASM1051, ASM1053 and ASM1153 devices <sigh>.
    
    2) Our detection logic to differentiate between ASM1051 and ASM1053 sees
       ASM1153 devices as ASM1051 because they have 32 streams like ASM1051 devs.
       Luckily the ASM1153 descriptors are not 100% identical, unlike the previous
       models the ASM1153 has bMaxPower == 0, so use that to differentiate it.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/storage/unusual_uas.h
+++ drivers/usb/storage/unusual_uas.h
@@ -141,14 +141,6 @@ UNUSUAL_DEV(0x152d, 0x0567, 0x0000, 0x9999,
 		USB_SC_DEVICE, USB_PR_DEVICE, NULL,
 		US_FL_NO_REPORT_OPCODES),
 
-/* Most ASM1051 based devices have issues with uas, blacklist them all */
-/* Reported-by: Hans de Goede <hdegoede@redhat.com> */
-UNUSUAL_DEV(0x174c, 0x5106, 0x0000, 0x9999,
-		"ASMedia",
-		"ASM1051",
-		USB_SC_DEVICE, USB_PR_DEVICE, NULL,
-		US_FL_IGNORE_UAS),
-
 /* Reported-by: David Kozub <zub@linux.fjfi.cvut.cz> */
 UNUSUAL_DEV(0x152d, 0x0578, 0x0000, 0x9999,
 		"JMicron",
