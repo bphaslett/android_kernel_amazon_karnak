1b92f995b6740a9d9d82f267e0f8217e53f29017
Nicolas Dichtel <nicolas.dichtel@6wind.com>
2015-01-26 22:28:14 +0100

    Resolve merge conflict for commit 33564bbb2cf1
    vxlan: setup the right link netns in newlink hdlr
    
    Rename the netns to src_net to avoid confusion with the netns where the
    interface stands. The user may specify IFLA_NET_NS_[PID|FD] to create
    a x-netns netndevice: IFLA_NET_NS_[PID|FD] points to the netns where the
    netdevice stands and src_net to the link netns.
    
    Note that before commit f01ec1c017de ("vxlan: add x-netns support"), it was
    possible to create a x-netns vxlan netdevice, but the netdevice was not
    operational.
    
    Fixes: f01ec1c017de ("vxlan: add x-netns support")
    Signed-off-by: Nicolas Dichtel <nicolas.dichtel@6wind.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/vxlan.c
+++ drivers/net/vxlan.c
@@ -2445,10 +2445,10 @@ static void vxlan_sock_work(struct work_struct *work)
 	dev_put(vxlan->dev);
 }
 
-static int vxlan_newlink(struct net *net, struct net_device *dev,
+static int vxlan_newlink(struct net *src_net, struct net_device *dev,
 			 struct nlattr *tb[], struct nlattr *data[])
 {
-	struct vxlan_net *vn = net_generic(net, vxlan_net_id);
+	struct vxlan_net *vn = net_generic(src_net, vxlan_net_id);
 	struct vxlan_dev *vxlan = netdev_priv(dev), *tmp;
 	struct vxlan_rdst *dst = &vxlan->default_dst;
 	__u32 vni;
@@ -2458,7 +2458,7 @@ static int vxlan_newlink(struct net *net, struct net_device *dev,
 	if (!data[IFLA_VXLAN_ID])
 		return -EINVAL;
 
-	vxlan->net = dev_net(dev);
+	vxlan->net = src_net;
 
 	vni = nla_get_u32(data[IFLA_VXLAN_ID]);
 	dst->remote_vni = vni;
@@ -2494,7 +2494,7 @@ static int vxlan_newlink(struct net *net, struct net_device *dev,
 	if (data[IFLA_VXLAN_LINK] &&
 	    (dst->remote_ifindex = nla_get_u32(data[IFLA_VXLAN_LINK]))) {
 		struct net_device *lowerdev
-			 = __dev_get_by_index(net, dst->remote_ifindex);
+			 = __dev_get_by_index(src_net, dst->remote_ifindex);
 
 		if (!lowerdev) {
 			pr_info("ifindex %d does not exist\n", dst->remote_ifindex);
